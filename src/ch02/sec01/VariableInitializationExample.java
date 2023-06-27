5407fcf333fc2a1702c854cc9ccf0ef2415d537b
devmeeple <devmeeple@gmail.com>
2023-06-27 21:37:10 +0900

    자바 언어 기초 - ch02. 변수와 타입

---
+++ src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,10 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        int value;
+//        int result = value + 10; value 변수를 초기화 하지 않았기에 오류 발생
+
+//        System.out.println("result = " + result);
+    }
+}
