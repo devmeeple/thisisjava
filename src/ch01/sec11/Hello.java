69aae21060b8cb7262447bdb7a9b42db0fdecae9
devmeeple <devmeeple@gmail.com>
2023-06-27 20:09:35 +0900

    자바 언어 기초 - ch01. 자바 시작하기

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,14 @@
+package ch01.sec11;
+
+/**
+ * @author devmeeple
+ */
+/*
+장 제목 : 1장 자바 시작하기
+작성일 : 2023.06.27
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
